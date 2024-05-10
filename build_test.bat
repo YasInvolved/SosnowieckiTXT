8b7a2053bc90118d444c3dffb1d4490b7ee4f55d
YasInvolved <szrtxm.op@gmail.com>
2024-05-10 02:31:06 +0200

    elytra update (wietnam)

--- build_test.bat
+++ build_test.bat
@@ -1,2 +1,2 @@
 @echo off
-winrar a -afzip -r "%appdata%/.minecraft/resourcepacks/sosnowiecki-txt-test.zip" assets pack.mcmeta pack.png
+winrar a -afzip -r "%appdata%\PrismLauncher\instances\1.20.1\.minecraft\resourcepacks\sosnowiecki-txt-test.zip" assets pack.mcmeta pack.png
